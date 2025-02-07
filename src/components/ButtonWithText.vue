b3fd37f0b998b8de23a121cc13ba84db53b030a4
Hunter <i.am.hunter.777.ih@gmail.com>
2025-02-07 20:05:31 +0300

    Updated component 'ButtonWithText' - updated styles to lang 'scss'

--- src/components/ButtonWithText.vue
+++ src/components/ButtonWithText.vue
@@ -31,46 +31,52 @@ const props = defineProps<Props>()
 const emit = defineEmits(['click'])
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 .button {
   display: inline-flex;
   align-items: center;
   justify-content: center;
-  padding: 0 1.5rem;
+  padding: 1.5rem 1.5rem;
   border: none;
   border-radius: 2rem;
-  font-size: 1.25rem;
   cursor: pointer;
   transition: background-color 0.2s ease;
   color: var(--white);
   background-color: var(--green-light);
-}
-.button:hover {
-  background-color: var(--green-middle);
-}
-.button:active {
-  background-color: var(--green-dark);
-}
-.button--disabled {
-  background-color: var(--gray);
-  cursor: not-allowed;
-}
+  &:hover {
+    background-color: var(--green-middle);
+  }
+  &:active {
+    background-color: var(--green-dark);
+  }
+  &--disabled {
+    background-color: var(--gray);
+    cursor: not-allowed;
+  }
 
-.button--icon-text {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-  gap: 0.75rem;
-}
-.button--round {
-  border-radius: 50%;
-  width: 3.5rem;
-  height: 3.5rem;
-  padding: 0;
-}
-.button__icon {
-  margin-right: 0.5rem;
-  display: inline-flex;
+  &--icon-text {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    gap: 0.75rem;
+  }
+  &--round {
+    border-radius: 50%;
+    width: 3.5rem;
+    height: 3.5rem;
+    padding: 0;
+  }
+  &__icon {
+    margin-right: 0.5rem;
+    display: inline-flex;
+  }
+  &__text {
+    font-family: var(--font-family);
+    font-weight: 500;
+    font-size: 1.25rem;
+    line-height: 160%;
+    color: var(--white);
+  }
 }
 </style>
