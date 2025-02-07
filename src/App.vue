223afec071487241ab87f18592a3c3d837d12807
Hunter <i.am.hunter.777.ih@gmail.com>
2025-02-07 19:16:34 +0300

    Updated new component 'NoteList'

--- src/App.vue
+++ src/App.vue
@@ -20,9 +20,6 @@
       <p>Описание: {{ description }}</p>
     </div>
     <div>
-      <NoteComponent :note="myNote" :has-text="false" @delete="handleDelete" />
-    </div>
-    <div>
       <h1>Мои заметки</h1>
       <NotesList />
     </div>
@@ -34,21 +31,9 @@ import { ref } from 'vue'
 import { RouterLink, RouterView } from 'vue-router'
 import ButtonWithText from '@/components/ButtonWithText.vue'
 import TextareaComponent from '@/components/TextareaComponent.vue'
-import NoteComponent from '@/components/NoteComponent.vue'
 import NotesList from '@/components/NotesList.vue'
 
 const description = ref('')
-
-const myNote = ref({
-  id: '123',
-  title: 'Моя первая заметка',
-  text: 'Текст моей первой заметки.',
-})
-
-const handleDelete = (id: string) => {
-  alert(`Удалить заметку с id: ${id}`)
-  // Здесь будет логика удаления заметки
-}
 </script>
 
 <style scoped></style>
--- src/components/NotesList.vue
+++ src/components/NotesList.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="notes-list">
-    <Note :note="notes[0]" :has-text="true" @delete="handleDelete" />
+    <NoteComponent :note="notes[0]" :has-text="true" @delete="handleDelete" />
     <!-- цикл v-for для отображения всех заметок -->
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
-import Note from './Note.vue'
+import NoteComponent from './NoteComponent.vue'
 
 interface NoteType {
   id: string
